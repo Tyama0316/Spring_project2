6c9cec89568ffb550433a04a6d3d2e406037abe8
Tyama0316 <takayama19990316@gmail.com>
2024-06-12 21:05:59 +0900

    02GETとPOST 計算機課題 thirdcommit

--- src/main/java/com/example/demo/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/demo/calculator/controller/CalculatorController.java
@@ -15,25 +15,27 @@ public class CalculatorController {
     @Autowired
     private CalculatorService calculatorService;
 
-    @GetMapping("/calculator")
+    @GetMapping("/calculate")
     public String showCalculator() {
-        return "calculator";
+        return "calculator.html";
     }
 
-    @PostMapping("/calculator")
+    @PostMapping("/calculate")
     public String calculate(@RequestParam("number1") int number1, 
                             @RequestParam("number2") int number2, 
                             @RequestParam("operation") String operation, 
                             Model model) {
+    	boolean isError = false;
         try {
             int result = calculatorService.calculate(number1, number2, operation);
             model.addAttribute("result", result);
-            model.addAttribute("isError", false);
+            model.addAttribute("isError", isError);
         } catch (IllegalArgumentException e) {
+        	isError = true;
             model.addAttribute("result", "error");
-            model.addAttribute("isError", true);
+            model.addAttribute("isError", isError);
             model.addAttribute("errorMessage", e.getMessage());
         }
-        return "calculator";
+        return "calculator.html";
     }
 }
